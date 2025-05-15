81e5885f7ea65f7ea3da960b1fa32b1ad2668112
Sam Stockman <ss15859@bristol.ac.uk>
2025-05-15 23:48:12 +0100

    Add simulation scripts for ComCat, SCEDC, SaltonSea, SanJac, and WHITE datasets with updated parameters

--- scripts/simulate_WHITE.sh
+++ scripts/simulate_WHITE.sh
@@ -16,7 +16,7 @@ total_epochs=2000
 cuda_id=0
 marked_output=1
 
-
 save_path=./ModelSave/dataset_${dataset}_model_${model}_sigma_${sigma_time}_${sigma_loc}_log_${log_normalization}_seed_${seed}_dim_${cond_dim}_lambda_${loss_lambda}_seq_len_${seq_len}_marked_output_${marked_output}/
 
-python app.py --dataset ${dataset} --mode train --save_path ${save_path} --timesteps ${timesteps} --samplingsteps ${samplingsteps} --batch_size ${batch_size} --cuda_id ${cuda_id} --total_epochs ${total_epochs} --seq_len ${seq_len} --catalog_path ${catalog_path} --Mcut ${Mcut} --auxiliary_start ${auxiliary_start} --train_nll_start ${train_nll_start} --val_nll_start ${val_nll_start} --test_nll_start ${test_nll_start} --test_nll_end ${test_nll_end} --marked_output ${marked_output}
+
+python app.py --day_number $1 --batch_size $2 --save_path ${save_path} --dataset ${dataset} --mode sample --model ${model} --timesteps ${timesteps}  --samplingsteps ${samplingsteps}  --seq_len ${seq_len} --catalog_path ${catalog_path} --Mcut ${Mcut} --auxiliary_start ${auxiliary_start} --train_nll_start ${train_nll_start} --val_nll_start ${val_nll_start} --test_nll_start ${test_nll_start} --test_nll_end ${test_nll_end} --marked_output ${marked_output} --weight_path ${save_path}model_best.pkl
